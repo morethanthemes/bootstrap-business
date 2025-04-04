bd6804589b8c8919fa0ebb0faaf2d69d252e234f
skech <soukri@morethanthemes.com>
2025-04-04 19:14:46 +0300

    chore(): remove old code and files from the branch

--- d8/site/themes/contrib/bootstrap_business/js/custom/toggle.js
+++
@@ -1,9 +0,0 @@
-function hidePreHeader(){
-	jQuery(".toggle-control").html("<a href=\"javascript:showPreHeader()\"><span class=\"glyphicon glyphicon-plus\"></span></a>");
-	jQuery("#pre-header-inside").slideUp("fast");
-	}
-
-function showPreHeader() {
-    jQuery(".toggle-control").html("<a href=\"javascript:hidePreHeader()\"><span class=\"glyphicon glyphicon-minus\"></span></a>");
-    jQuery("#pre-header-inside").slideDown("fast");
-}
